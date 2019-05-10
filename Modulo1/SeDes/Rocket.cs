79fbe2aa830a0ac82e785c80976987bb8055475d
João Paulo Ferreira de Souza <joao.ferreiradesouza@hotmail.com>
2019-05-10 11:58:19 -0300

    Lab 1&2 Demo Modulo1

---
+++ Modulo1/SeDes/Rocket.cs
@@ -0,0 +1,20 @@
+namespace SeDes
+{
+    class Rocket
+    {
+        public int Id { get; set; }
+        public string Builder { get; set; }
+        public string Target { get; set; }
+        public double Speed { get; set; }
+
+
+    }
+
+     public class UFO {
+        //public string Builder { get; set; }
+        public string Target { get; set; }
+        //public double Speed { get; set; }
+        //public int Id { get; set; }
+    }
+
+}
